2ddb988ddc78b4a45e8ce949a71d673d8c2cc7fc
Victor Besyakov <victor.besyakov@gmail.com>
2016-12-19 11:25:37 -0500

    modified:   doc/Data/FileInfo.nd 	modified:   doc/Data/IndexInfo.nd 	modified:   doc/Data/SymbolTable.nd 	modified:   doc/files/utils/src/svcs_c2sv_mapping-h.html 	modified:   doc/index/General.html 	modified:   doc/javascript/searchdata.js 	modified:   doc/search/GeneralE.html 	modified:   doc/search/GeneralF.html 	modified:   doc/search/GeneralT.html 	modified:   utils/src/svcs_c2sv_mapping.h

--- utils/src/svcs_c2sv_mapping.h
+++ utils/src/svcs_c2sv_mapping.h
@@ -8,27 +8,13 @@
                See LICENSE file in the project root for full license information.  
  Description : "C" to system verilog mapping util
                System Verilog client server handshake (SVCS)
-******************************************************
-Data Types:
-elements:         integer,  double, char
-vectors:          integers, doubles , string
-array/composite:  integer vectors , double vectors, messages , structure
--------------------------------------------------------
-trnx -> header  ->  trnx_atribute  - hash/random double
-                    trnx_type      - hash double
-                    trnx_id        - random double
-        payload -> size - int , >0
-                   data
- ============================================================================
- */
+==============================================================================
+*/
+
 #ifndef SVCS_C2SV_MAPPING_H_
 #define SVCS_C2SV_MAPPING_H_
 
 #include  "svcs_client_server.h"
-
-#define MY_HOST "localhost"
-#define BUFSIZE   1024
-#define MAXBUFLEN 1024
 
 //-------------
 //prototypes
@@ -38,75 +24,82 @@ trnx -> header  ->  trnx_atribute  - hash/random double
 
 //Section: Integer atom type
 
-/* shortint */
-/* int */
-/* longint */
-/* byte */
-/* integer  */
-/* time  */
+
+/*
+
+(start code)
+
+shortint 
+int 
+longint 
+byte 
+integer  
+  time  
+  
+  (end)
+*/
 
 //Section:  Integer vector type
 
-/* bit[N:0] */
-/* reg[N:0]
-/* logic[N:0] */
+ /*
+   (start code)
+   
+   bit[N:0] 
+   reg[N:0]
+   logic[N:0] 
+   
+   (end)
+   
+*/
 
-//Section: Non integer type IEEE 754
-/* shortreal */
-/* real */
-/* realtime */
+ //Section: Non integer type IEEE 754
 
-/* string */
-/* string[N] */
-/* chandle */
-
-//Section: Aggregate data types
-
-/* Unpacked structures */
-
-/* Packed structures */
-
-/* Unpacked union */
-/* Packed  union */
-
-/* Unpacked arrays */
-
-/* Packed arrays */
-
-/* enum */
-
-/* fixed-size arrays */
-/* dynamic arrays */
-/* associative arrays */
-/* queues */
-
-/* multi-dimensioned packed/unpacked arrays */
-/* bit-stream */
-
-
-
-///////////////////////////////
-//Section: Example Template DEBUG !!!!!
-
-/*
- Section: TODO
-
-(start code)
-   TODO
- (end)
-
- */
-
-/*
-  Function: TODO
-  Parameters:
-   TODO
- */
-
+ /*
+   
+ (start code) 
  
+ shortreal 
+ real 
+ realtime 
+ 
+ string 
+ string[N] 
+ chandle 
+ 
+(end)
 
+ */
 
+ //Section: Aggregate data types
 
+ /*
+
+ (start code) 
+ 
+  Unpacked structures 
+  
+  Packed structures 
+  
+  Unpacked union 
+  Packed  union 
+  
+  Unpacked arrays 
+  
+  Packed arrays 
+
+  enum 
+  
+  fixed-size arrays 
+  dynamic arrays 
+  associative arrays 
+  queues 
+  
+  multi-dimensioned packed/unpacked arrays 
+  bit-stream 
+  
+  (end)
+
+ */
 
 #endif
 
