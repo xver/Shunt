3853085686a40195454c02e7a44814f9d5109d6a
Victor Besyakov <victor.besyakov@gmail.com>
2017-04-01 15:45:12 -0400

    new file:   doc/index/General2.html 	new file:   utils/src/utils.c

---
+++ utils/src/utils.c
@@ -0,0 +1,20 @@
+/*
+ ============================================================================
+ Name        : utils.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+//#include "svcs_primitives.h"
+//#include "svcs_primitives.c"
+//#include "svcs_client_server.h"
+
+#include "svcs_user_api.h"
+
+
+int main(void) {
+	puts("SVCS utils compile"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}
