d054e11b83439d2fca04e4fa6f70666bf67a14d5
Victor Besyakov <victor.besyakov@gmail.com>
2017-04-02 12:07:15 -0400

    new file:   dpi/svcs_dpi/src/svcs_dpi.c 	new file:   dpi/svcs_dpi/src/svcs_dpi.h 	new file:   tests/sv/primitives/client/src/svtest_prim_client.sv 	new file:   tests/sv/primitives/server/src/svtest_prim_sever.sv

---
+++ dpi/svcs_dpi/src/svcs_dpi.c
@@ -0,0 +1,19 @@
+ /*
+ =======svcs_cs==================================================
+ File        : svcs_dpi.c
+ Author      : Victor Besyakov
+ Version     : 0.0.0
+ Copyright (c) 2016 IC Verimeter. All rights reserved.
+               Licensed under the MIT License.
+               See LICENSE file in the project root for full license information.
+ Description : User API  for
+               System Verilog client server handshake dpi (SVCS)
+******************************************************
+ */
+
+
+#include "svcs_dpi.h"
+int main(void) {
+	puts("ctest_user_api_server"); /* prints ctest_user_api_server */
+	return EXIT_SUCCESS;
+}
---
+++ dpi/svcs_dpi/src/svcs_dpi.h
@@ -0,0 +1,19 @@
+/*
+=======svcs_cs==================================================
+ File        : svcs_dpi.h
+ Author      : Victor Besyakov
+ Version     : 0.0.0
+ Copyright (c) 2016 IC Verimeter. All rights reserved.
+               Licensed under the MIT License.
+               See LICENSE file in the project root for full license information.
+ Description : User API  for
+               System Verilog client server handshake (SVCS)
+******************************************************
+*/
+#ifndef SVCS_DPI_H_
+#define SVCS_DPI_H_
+
+#include "svdpi.h"
+#include "svcs_user_api.h"
+
+#endif /* SVCS_DPI_H_ */
