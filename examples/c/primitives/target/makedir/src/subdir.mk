daac4cd1d4cdfde5ef97923bca3d2594fb9a97d4
Victor Besyakov <victor.besyakov@gmail.com>
2017-09-27 22:25:26 -0400

    deleted:    all_files 	new file:   bin/placeholder 	new file:   examples/c/client_server/initiator/makedir/src/subdir.mk 	new file:   examples/c/client_server/target/makedir/src/subdir.mk 	new file:   examples/c/primitives/initiator/makedir/src/subdir.mk 	new file:   examples/c/primitives/target/makedir/src/subdir.mk 	new file:   examples/c/user_api/initiator/makedir/src/subdir.mk 	new file:   examples/c/user_api/target/makedir/src/subdir.mk 	new file:   examples/sv/handshake/initiator/bin/placeholder 	modified:   examples/sv/handshake/initiator/makedir/Makefile 	modified:   examples/sv/handshake/initiator/makedir/run 	new file:   examples/sv/handshake/target/bin/placeholder 	modified:   examples/sv/handshake/target/makedir/Makefile 	modified:   examples/sv/handshake/target/makedir/run 	new file:   examples/sv/primitives/initiator/bin/placeholder 	modified:   examples/sv/primitives/initiator/makedir/Makefile 	modified:   examples/sv/primitives/initiator/makedir/run 	new file:   examples/sv/primitives/target/bin/placeholder 	modified:   examples/sv/primitives/target/makedir/Makefile 	modified:   examples/sv/primitives/target/makedir/run 	new file:   examples/sv/sv2c/bin/placeholder 	new file:   examples/sv/sv2c/initiator/bin/placeholder 	modified:   examples/sv/sv2c/initiator/makedir/Makefile 	modified:   examples/sv/sv2c/initiator/makedir/run 	new file:   examples/sv/sv2c/target/bin/placeholder 	modified:   examples/sv/sv2c/target/makedir/Makefile 	modified:   examples/sv/sv2c/target/makedir/run 	new file:   utils/makedir/c/src/subdir.mk 	new file:   utils/makedir/dpi/src/subdir.mk

---
+++ examples/c/primitives/target/makedir/src/subdir.mk
@@ -0,0 +1,24 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../src/primC_Target.c 
+
+OBJS += \
+./src/primC_Target.o 
+
+C_DEPS += \
+./src/primC_Target.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/%.o: ../src/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C Compiler'
+	gcc -I"${SHUNT_HOME}/utils/c/include" -include"${SHUNT_HOME}/utils/c/include/shunt_primitives.h" -include"${SHUNT_HOME}/utils/c/src/shunt_primitives.c" -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
