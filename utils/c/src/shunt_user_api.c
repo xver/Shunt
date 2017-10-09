6feac9c697c975a0c1cb01565b5448350952f07b
Victor Besyakov <victor.besyakov@gmail.com>
2017-10-09 12:59:53 -0400

    update

--- utils/c/src/shunt_user_api.c
+++ utils/c/src/shunt_user_api.c
@@ -59,56 +59,56 @@ int shunt_api_send    (int sockid,cs_header* h_trnx,...) {
   case SHUNT_SHORTINT: 
     Short_ = va_arg (ap,short int* );
     if (main_data_type_ !=  SHUNT_A_STRUCTURE)  Result_ = shunt_cs_send_shortV(sockid,h_trnx,Short_);
-    else   Result_ = shunt_cs_send_shortA(sockid,h_trnx->n_payloads,h_data_,Short_);
+    //else   Result_ = shunt_cs_send_shortA(sockid,h_trnx->n_payloads,h_data_,Short_);
     break;
     
   case  SHUNT_INT :
     Int_ = va_arg (ap,int* );
     if (main_data_type_ !=  SHUNT_A_STRUCTURE)  Result_ = shunt_cs_send_intV(sockid,h_trnx,Int_);
-    else   Result_ = shunt_cs_send_intA(sockid,h_trnx->n_payloads,h_data_,Int_);
+    //else   Result_ = shunt_cs_send_intA(sockid,h_trnx->n_payloads,h_data_,Int_);
     break;
 
   case SHUNT_LONGINT: 
     Long_ = va_arg (ap,long int* );
     if (main_data_type_ !=  SHUNT_A_STRUCTURE)  Result_ = shunt_cs_send_longV(sockid,h_trnx,Long_);
-    else   Result_ = shunt_cs_send_longA(sockid,h_trnx->n_payloads,h_data_,Long_);
+    //else   Result_ = shunt_cs_send_longA(sockid,h_trnx->n_payloads,h_data_,Long_);
     break;
    
   case  SHUNT_STRING:
   case  SHUNT_BYTE:
     String_ = va_arg (ap,char* );
     if (main_data_type_ !=  SHUNT_A_STRUCTURE) Result_ = shunt_cs_send_byteV(sockid,h_trnx,String_);
-    else   Result_ = shunt_cs_send_byteA(sockid,h_trnx->n_payloads,h_data_,String_);
+    //else   Result_ = shunt_cs_send_byteA(sockid,h_trnx->n_payloads,h_data_,String_);
     break;
    
    case  SHUNT_BIT :
      Bit_ = va_arg (ap,svBitVecVal*);
      if (main_data_type_ !=  SHUNT_A_STRUCTURE)  Result_ = shunt_cs_send_bitN(sockid,h_trnx,Bit_);
-     else   Result_ = shunt_cs_send_bitA(sockid,h_trnx->n_payloads,h_data_,Bit_);
+     //else   Result_ = shunt_cs_send_bitA(sockid,h_trnx->n_payloads,h_data_,Bit_);
      break;
    
   case SHUNT_INTEGER: 
     Integer_ = va_arg (ap,svLogicVecVal* );
     if (main_data_type_ !=  SHUNT_A_STRUCTURE)  Result_ = shunt_cs_send_integerV(sockid,h_trnx,Integer_);
-    else   Result_ = shunt_cs_send_integerA(sockid,h_trnx->n_payloads,h_data_,Integer_);
+    //else   Result_ = shunt_cs_send_integerA(sockid,h_trnx->n_payloads,h_data_,Integer_);
     break;
   
   case SHUNT_REG: 
     Reg_ = va_arg (ap,svLogicVecVal* );
     if (main_data_type_ !=  SHUNT_A_STRUCTURE)  Result_ = shunt_cs_send_regN(sockid,h_trnx,Reg_);
-    else   Result_ = shunt_cs_send_regA(sockid,h_trnx->n_payloads,h_data_,Reg_);
+    //else   Result_ = shunt_cs_send_regA(sockid,h_trnx->n_payloads,h_data_,Reg_);
     break;
     
   case  SHUNT_REAL :
     Double_ = va_arg (ap,double* );
     if (main_data_type_ !=  SHUNT_A_STRUCTURE)  {  Result_ = shunt_cs_send_doubleV(sockid,h_trnx,Double_);}
-    else   Result_ = shunt_cs_send_doubleA(sockid,h_trnx->n_payloads,h_data_,Double_);
+    //else   Result_ = shunt_cs_send_doubleA(sockid,h_trnx->n_payloads,h_data_,Double_);
     break;
  
   case  SHUNT_SHORTREAL :
     Float_ = va_arg (ap,float* );
     if (main_data_type_ !=  SHUNT_A_STRUCTURE)  {  Result_ = shunt_cs_send_floatV(sockid,h_trnx,Float_);}
-    else   Result_ = shunt_cs_send_floatA(sockid,h_trnx->n_payloads,h_data_,Float_);
+    //else   Result_ = shunt_cs_send_floatA(sockid,h_trnx->n_payloads,h_data_,Float_);
     break;     
 
   default :
@@ -165,56 +165,56 @@ int shunt_api_recv    (int sockid,cs_header* h_trnx,...) {
   case SHUNT_SHORTINT: 
     Short_ = va_arg (ap,short int* );
     if (main_data_type_ !=  SHUNT_A_STRUCTURE)  Result_ = shunt_cs_recv_shortV(sockid,h_trnx,Short_);
-    else   Result_ = shunt_cs_recv_shortA(sockid,h_trnx->n_payloads,h_data_,Short_);
+    //else   Result_ = shunt_cs_recv_shortA(sockid,h_trnx->n_payloads,h_data_,Short_);
     break;
     
   case  SHUNT_INT :
     Int_ = va_arg (ap,int* );
     if (main_data_type_ !=  SHUNT_A_STRUCTURE)  Result_ = shunt_cs_recv_intV(sockid,h_trnx,Int_);
-    else   Result_ = shunt_cs_recv_intA(sockid,h_trnx->n_payloads,h_data_,Int_);
+    //else   Result_ = shunt_cs_recv_intA(sockid,h_trnx->n_payloads,h_data_,Int_);
     break;
 
   case SHUNT_LONGINT: 
     Long_ = va_arg (ap,long int* );
     if (main_data_type_ !=  SHUNT_A_STRUCTURE)  Result_ = shunt_cs_recv_longV(sockid,h_trnx,Long_);
-    else   Result_ = shunt_cs_recv_longA(sockid,h_trnx->n_payloads,h_data_,Long_);
+    //else   Result_ = shunt_cs_recv_longA(sockid,h_trnx->n_payloads,h_data_,Long_);
     break;
    
   case  SHUNT_STRING:
   case  SHUNT_BYTE:
     String_ = va_arg (ap,char* );
     if (main_data_type_ !=  SHUNT_A_STRUCTURE) Result_ = shunt_cs_recv_byteV(sockid,h_trnx,String_);
-    else   Result_ = shunt_cs_recv_byteA(sockid,h_trnx->n_payloads,h_data_,String_);
+    //else   Result_ = shunt_cs_recv_byteA(sockid,h_trnx->n_payloads,h_data_,String_);
     break;
    
    case  SHUNT_BIT :
      Bit_ = va_arg (ap,svBitVecVal*);
      if (main_data_type_ !=  SHUNT_A_STRUCTURE)  Result_ = shunt_cs_recv_bitN(sockid,h_trnx,Bit_);
-     else   Result_ = shunt_cs_recv_bitA(sockid,h_trnx->n_payloads,h_data_,Bit_);
+     //else   Result_ = shunt_cs_recv_bitA(sockid,h_trnx->n_payloads,h_data_,Bit_);
      break;
    
   case SHUNT_INTEGER: 
     Integer_ = va_arg (ap,svLogicVecVal* );
     if (main_data_type_ !=  SHUNT_A_STRUCTURE)  Result_ = shunt_cs_recv_integerV(sockid,h_trnx,Integer_);
-    else   Result_ = shunt_cs_recv_integerA(sockid,h_trnx->n_payloads,h_data_,Integer_);
+    //else   Result_ = shunt_cs_recv_integerA(sockid,h_trnx->n_payloads,h_data_,Integer_);
     break;
   
   case SHUNT_REG: 
     Reg_ = va_arg (ap,svLogicVecVal* );
     if (main_data_type_ !=  SHUNT_A_STRUCTURE)  Result_ = shunt_cs_recv_regN(sockid,h_trnx,Reg_);
-    else   Result_ = shunt_cs_recv_regA(sockid,h_trnx->n_payloads,h_data_,Reg_);
+    //else   Result_ = shunt_cs_recv_regA(sockid,h_trnx->n_payloads,h_data_,Reg_);
     break;
     
   case  SHUNT_REAL :
     Double_ = va_arg (ap,double* );
     if (main_data_type_ !=  SHUNT_A_STRUCTURE)  {  Result_ = shunt_cs_recv_doubleV(sockid,h_trnx,Double_);}
-    else   Result_ = shunt_cs_recv_doubleA(sockid,h_trnx->n_payloads,h_data_,Double_);
+    //else   Result_ = shunt_cs_recv_doubleA(sockid,h_trnx->n_payloads,h_data_,Double_);
     break;
  
   case  SHUNT_SHORTREAL :
     Float_ = va_arg (ap,float* );
     if (main_data_type_ !=  SHUNT_A_STRUCTURE)  {  Result_ = shunt_cs_recv_floatV(sockid,h_trnx,Float_);}
-    else   Result_ = shunt_cs_recv_floatA(sockid,h_trnx->n_payloads,h_data_,Float_);
+    //else   Result_ = shunt_cs_recv_floatA(sockid,h_trnx->n_payloads,h_data_,Float_);
     break;     
     
   default :
