fb2e3ac187ec23a23ee77d2f2deef5282a537930
xver <victor.besyakov@gmail.com>
2018-10-13 10:45:47 -0400

    Multi-Slave TCP IP initialisation intermediate release

--- examples/c/primitives/initiator/src/primC_Initiator.c
+++ examples/c/primitives/initiator/src/primC_Initiator.c
@@ -14,17 +14,17 @@
 
 int main(void) {
   puts("\nexample_prim_initiator start");
-  int  socket;
+  prim_socketid_struct  socket;
   int  port;
   int success = 1;
   
   port =       MY_PORT;
   socket= shunt_prim_init_initiator(port);
-  if (socket<0) {
+  if (socket.childfd<0) {
     printf("example_prim_initiator::FATAL ERROR");
     success = 0;
   }
-  printf("\nexample_prim_initiator::socket=%d", socket);
+  printf("\nexample_prim_initiator::socket.childfd=%d", socket.childfd);
   
   if (success>0) {
     puts("\nexample_prim_initiator start: Echo loopback initiator example start");
@@ -32,8 +32,8 @@ int main(void) {
     //INT example
     int Int = 0xaa5555aa;
     int IntLoopBack = 0x5a5a5a5a;
-    if(shunt_prim_send_int(socket,&Int)       <=0 ) success = 0;
-    if(shunt_prim_recv_int(socket,&IntLoopBack)<=0) success = 0;
+    if(shunt_prim_send_int(socket.childfd,&Int)       <=0 ) success = 0;
+    if(shunt_prim_recv_int(socket.childfd,&IntLoopBack)<=0) success = 0;
     if (IntLoopBack == Int && success >0)
       printf("\nint loopback pass");
     else {
@@ -44,8 +44,8 @@ int main(void) {
     //DOUBLE Example
     double Double = -1234567;
     double DoubleLoopBack = 9101112;
-    if(shunt_prim_send_double(socket,&Double)       <= 0 ) success = 0;
-    if(shunt_prim_recv_double(socket,&DoubleLoopBack)<= 0) success = 0;
+    if(shunt_prim_send_double(socket.childfd,&Double)       <= 0 ) success = 0;
+    if(shunt_prim_recv_double(socket.childfd,&DoubleLoopBack)<= 0) success = 0;
     
     if (DoubleLoopBack == Double && success >0)
       printf("\ndouble loopback pass");
@@ -56,8 +56,8 @@ int main(void) {
 
     char Byte = 'C';
     char ByteLoopBack = 'b';
-    if(shunt_prim_send_byte(socket,&Byte)       <= 0 ) success = 0;
-    if(shunt_prim_recv_byte(socket,&ByteLoopBack)<= 0) success = 0;
+    if(shunt_prim_send_byte(socket.childfd,&Byte)       <= 0 ) success = 0;
+    if(shunt_prim_recv_byte(socket.childfd,&ByteLoopBack)<= 0) success = 0;
     
     if (ByteLoopBack == Byte && success >0)
         printf("\nbyte loopback pass");
