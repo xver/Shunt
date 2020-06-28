77b5d6e4ac1c859f284f49db6de7e5ba87f4d458
xver <victor.besyakov@gmail.com>
2020-06-28 11:39:19 -0500

    update: linux utils doc

---
+++ doc/search/keywords/006600690078.js
@@ -0,0 +1,1 @@
+NDSearch.OnPrefixDataLoaded("fix",["Section"],[["Fixed",,[[,"Fixed size packet communication Data exchange",,,0,"File:shunt_user_api.h:Fixed_size_packet_communication_Data_exchange"]]]]);
---
+++ doc/search/keywords/00730069007a.js
@@ -0,0 +1,1 @@
+NDSearch.OnPrefixDataLoaded("siz",["Section"],[["size",,[[,"Fixed size packet communication Data exchange",,,0,"File:shunt_user_api.h:Fixed_size_packet_communication_Data_exchange"]]]]);
