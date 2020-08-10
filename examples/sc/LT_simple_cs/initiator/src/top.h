93bbdb4f20944bc61fd9efaf063301560c210151
xver <victor.besyakov@gmail.com>
2020-08-09 21:30:28 -0500

    add TLM 2.0 example

---
+++ examples/sc/LT_simple_cs/initiator/src/top.h
@@ -0,0 +1,27 @@
+#ifndef TOP_H
+#define TOP_H
+
+
+#include "initiator.h"
+#include "target.h"
+#include "shunt_user_api.h"
+
+SC_MODULE(Top)
+{
+  Initiator *initiator;
+  Memory    *memory;
+
+  SC_CTOR(Top)
+  {
+    // Instantiate components
+    initiator = new Initiator("initiator");
+    memory    = new Memory   ("memory");
+
+    // One initiator is bound directly to one target with no intervening bus
+
+    // Bind initiator socket to target socket
+    initiator->socket.bind( memory->socket );
+  }
+};
+
+#endif
