a7ba9fb444d81b0d44f6893bfb13fba2aeba3814
xver <victor.besyakov@gmail.com>
2020-12-13 09:27:48 -0600

    cleanup:remove trailing whitespaces

--- utils/dpi/include/shunt_verilator_dpi.svh
+++ utils/dpi/include/shunt_verilator_dpi.svh
@@ -6,28 +6,28 @@
  Licensed under the MIT License.
  See LICENSE file in the project root for full license information.
  Description : shunt verilator integration
- 
+
  Not supported DPI functions defines:
  Version 1.0.1 : Verilator 3.916 2017-11-25 rev verilator_3_916
- 
+
  System Verilog target initiator handshake (TCP/IP SystemVerilog SHUNT)
  ******************************************************
 */
 
 `ifndef SHUNT_VERILATOR_DPI_H
  `define  SHUNT_VERILATOR_DPI_H
-  
+
  `define NO_CS_DATA_HEADER_T
 
  `define NO_SHUNT_DPI_HS_RECV_BITN
  `define NO_SHUNT_DPI_HS_SEND_BITN
-     
+
  `define NO_SHUNT_DPI_HS_RECV_BYTE
  `define NO_SHUNT_DPI_HS_SEND_BYTE
 
  `define NO_SHUNT_DPI_HS_RECV_INT
  `define NO_SHUNT_DPI_HS_SEND_INT
- 
+
  `define NO_SHUNT_DPI_HS_RECV_INTEGER
  `define NO_SHUNT_DPI_HS_SEND_INTEGER
 
@@ -45,7 +45,7 @@
 
  `define NO_SHUNT_DPI_HS_RECV_SHORT
  `define NO_SHUNT_DPI_HS_SEND_SHORT
- 
+
  `define NO_SHUNT_DPI_RECV_BITN
  `define NO_SHUNT_DPI_SEND_BITN
 
@@ -70,7 +70,7 @@
 // Unsupported: SystemVerilog 2005 reserved word not implemented: shortreal
  `define NO_SHUNT_DPI_HS_RECV_SHORTREAL
  `define NO_SHUNT_DPI_HS_SEND_SHORTREAL
- 
+
 `define NO_SHUNT_DPI_SEND_TIME
 `define NO_SHUNT_DPI_RECV_TIME
 
