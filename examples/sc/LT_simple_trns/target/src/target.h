bc55fedb953ab5ef76cf5898f679b2d0e516de4c
xver <victor.besyakov@gmail.com>
2020-12-13 09:27:43 -0600

    cleanup:remove trailing whitespaces

--- examples/sc/LT_simple_trns/target/src/target.h
+++ examples/sc/LT_simple_trns/target/src/target.h
@@ -38,7 +38,7 @@ struct Memory: sc_module
   virtual void b_transport( tlm::tlm_generic_payload& trans, sc_time& delay )
   {
 
-    shunt_recv_b_transport(m_socket,trans,delay ); 
+    shunt_recv_b_transport(m_socket,trans,delay );
     tlm::tlm_command cmd = trans.get_command();
     sc_dt::uint64    adr = trans.get_address() / 4;
     unsigned char*   ptr = trans.get_data_ptr();
