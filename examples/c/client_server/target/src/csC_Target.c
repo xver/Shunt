93e843940e0d6d2038690047e6be2f67622396fe
xver <victor.besyakov@gmail.com>
2020-12-13 09:27:30 -0600

    cleanup:remove trailing whitespaces

--- examples/c/client_server/target/src/csC_Target.c
+++ examples/c/client_server/target/src/csC_Target.c
@@ -1,14 +1,14 @@
 
-/* 
+/*
    ============================================================================
    File        : csC_Target.c
    Version     : 1.0.0
-   Copyright (c) 2016-2017 IC Verimeter. All rights reserved.  
-   Licensed under the MIT License. 
-   See LICENSE file in the project root for full license information.  
+   Copyright (c) 2016-2017 IC Verimeter. All rights reserved.
+   Licensed under the MIT License.
+   See LICENSE file in the project root for full license information.
    Description :  TCP/IP SystemVerilog SHUNT
                   An example of client-server functions-Target(Client)
-   
+
 */
 
 #include "shunt_client_server.h"
@@ -20,36 +20,36 @@ int main(void) {
   char *hostname;
   int port;
   int success=1;
-  
+
   //SVCV_INSTR_HASH_INDEX_DEFINE;
-  
+
   port = MY_PORT;
   hostname =   MY_HOST;
   //
   socket= shunt_prim_init_target(port,hostname);
-  
+
   if (socket<0) {
     printf("\ncsC_Target::FATAL ERROR");
     success=0;
   }
-  
+
   if (success>0) {
-    
+
     // Headers  Tests
-    
+
     // trnx_header test
     cs_header h_trnx;
-    
+
     //recv
     if (shunt_cs_recv_header(socket,&h_trnx)<= 0) success = 0;
     if (success == 0 )  printf("\n target trnx_header fail to recv");
     //send loopback
     if (shunt_cs_send_header(socket,&h_trnx)<= 0) success = 0;
     if (success == 0 )  printf("\n target trnx_header fail to send");
-    
+
     // data_header test
     cs_data_header h_data;
-    
+
     //recv
     h_data.trnx_payload_sizes = malloc(h_trnx.n_payloads*sizeof(int));
     if (shunt_cs_recv_data_header(socket,h_trnx.n_payloads,&h_data)<= 0) success = 0;
@@ -57,12 +57,12 @@ int main(void) {
     //send loopback
     if (shunt_cs_send_data_header(socket,h_trnx.n_payloads,&h_data)<= 0) success = 0;
     if (success == 0 )  printf("\n target data_header fail to send");
-    
+
     //     INT Test
-        
+
     //Int vector
     int* IntV;
-    
+
     //recv
     //header
     if (shunt_cs_recv_header(socket,&h_trnx)<= 0) success = 0;
@@ -71,7 +71,7 @@ int main(void) {
     IntV = (int *)malloc(h_trnx.n_payloads* sizeof(int));
     if (shunt_cs_recv_intV(socket,&h_trnx,IntV)<=0) success = 0;
     if (success == 0 )  printf("\n target Int data fail to recv");
-    
+
     //send loopback
     //header
     if (shunt_cs_send_header(socket,&h_trnx)<= 0) success = 0;
@@ -79,15 +79,15 @@ int main(void) {
     //data
     if (shunt_cs_send_intV(socket,&h_trnx,IntV)<=0) success = 0;
     if (success == 0 )  printf("\n target Int data fail to send");
-    
-    
-    ////////////////////////////////////    
-    
+
+
+    ////////////////////////////////////
+
     //Double Test
-           
+
     //Double vector
     double* DoubleV;
-    
+
     //recv
     //header
     if (shunt_cs_recv_header(socket,&h_trnx)<= 0) success = 0;
@@ -96,7 +96,7 @@ int main(void) {
     DoubleV = (double *)malloc(h_trnx.n_payloads* sizeof(double));
     if (shunt_cs_recv_doubleV(socket,&h_trnx,DoubleV)<=0) success = 0;
     if (success == 0 )  printf("\n target Double data fail to recv");
-    
+
     //send loopback
     //header
     if (shunt_cs_send_header(socket,&h_trnx)<= 0) success = 0;
@@ -104,7 +104,7 @@ int main(void) {
     //data
     if (shunt_cs_send_doubleV(socket,&h_trnx,DoubleV)<=0) success = 0;
     if (success == 0 )  printf("\n target Double data fail to send");
-    
+
     //String
     //char* msg = "target: recv double_header";
     char* String;
@@ -117,7 +117,7 @@ int main(void) {
 
     if(shunt_cs_recv_byteV(socket,&h_trnx,String)<=0) success = 0;
     if (success == 0 )  printf("\n target String data fail to recv");
-    
+
     //send loopback
     //header
     if (shunt_cs_send_header(socket,&h_trnx)<= 0) success = 0;
@@ -125,18 +125,18 @@ int main(void) {
     //data
     if (shunt_cs_send_byteV(socket,&h_trnx,String)<=0) success = 0;
     if (success == 0 )  printf("\n target String data fail to send");
-    
-    
-        
+
+
+
     ////////////////////////////////////
 
-    
+
     //puts("\ncsC_Target end");
   }
-  
+
   if ( success >0)
     return EXIT_SUCCESS;
-  else 
+  else
     return EXIT_FAILURE;
 }
 
