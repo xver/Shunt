28bf4236934f852f050323b1214ffa13c8bf294c
xver <victor.besyakov@gmail.com>
2020-12-13 09:27:50 -0600

    cleanup:remove trailing whitespaces

--- utils/makedir/sources.mk
+++ utils/makedir/sources.mk
@@ -2,14 +2,14 @@
 # Automatically-generated file. Do not edit!
 ################################################################################
 
-O_SRCS := 
-C_SRCS := 
-S_UPPER_SRCS := 
-OBJ_SRCS := 
-ASM_SRCS := 
-OBJS := 
-C_DEPS := 
-LIBRARIES := 
+O_SRCS :=
+C_SRCS :=
+S_UPPER_SRCS :=
+OBJ_SRCS :=
+ASM_SRCS :=
+OBJS :=
+C_DEPS :=
+LIBRARIES :=
 
 # Every subdirectory with source files must be described here
 SUBDIRS := \
