10e31f76e7fec5ffbee26f8d7b41a48ebf496176
xver <victor.besyakov@gmail.com>
2020-12-13 09:27:50 -0600

    cleanup:remove trailing whitespaces

--- utils/makedir/c/src/subdir.mk
+++ utils/makedir/c/src/subdir.mk
@@ -2,21 +2,21 @@
 # Automatically-generated file. Do not edit!
 ################################################################################
 
-# Add inputs and outputs from these tool invocations to the build variables 
+# Add inputs and outputs from these tool invocations to the build variables
 C_SRCS += \
 ../c/src/shunt_client_server.c \
 ../c/src/shunt_primitives.c \
-../c/src/shunt_user_api.c 
+../c/src/shunt_user_api.c
 
 OBJS += \
 ./c/src/shunt_client_server.o \
 ./c/src/shunt_primitives.o \
-./c/src/shunt_user_api.o 
+./c/src/shunt_user_api.o
 
 C_DEPS += \
 ./c/src/shunt_client_server.d \
 ./c/src/shunt_primitives.d \
-./c/src/shunt_user_api.d 
+./c/src/shunt_user_api.d
 
 
 # Each subdirectory must supply rules for building sources it contributes
