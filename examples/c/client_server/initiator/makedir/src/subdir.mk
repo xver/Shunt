eac8979c17040c1fb3e444c9b3c76fa0e3d19f96
xver <victor.besyakov@gmail.com>
2020-12-13 09:27:28 -0600

    cleanup:remove trailing whitespaces

--- examples/c/client_server/initiator/makedir/src/subdir.mk
+++ examples/c/client_server/initiator/makedir/src/subdir.mk
@@ -2,15 +2,15 @@
 # Automatically-generated file. Do not edit!
 ################################################################################
 
-# Add inputs and outputs from these tool invocations to the build variables 
+# Add inputs and outputs from these tool invocations to the build variables
 C_SRCS += \
-../src/csC_Initiator.c 
+../src/csC_Initiator.c
 
 OBJS += \
-./src/csC_Initiator.o 
+./src/csC_Initiator.o
 
 C_DEPS += \
-./src/csC_Initiator.d 
+./src/csC_Initiator.d
 
 
 # Each subdirectory must supply rules for building sources it contributes
