84fc8032f84965d5f550a4fa5fc514a2eabc1f2a
xver <victor.besyakov@gmail.com>
2020-12-13 09:27:43 -0600

    cleanup:remove trailing whitespaces

--- examples/sv/handshake/includes/cs_common.svh
+++ examples/sv/handshake/includes/cs_common.svh
@@ -2,4 +2,4 @@
 `define MY_PORT  3450
 `define V_SIZE   5
 `define STRING_MESSAGE "server function int string_loopback_test(int socket_id)\0"
-`define STRING_MESSAGE1 "SERVER FUNCTION INT STRING_LOOPBACK_TEST(INT SOCKET_ID)\0" 
+`define STRING_MESSAGE1 "SERVER FUNCTION INT STRING_LOOPBACK_TEST(INT SOCKET_ID)\0"
