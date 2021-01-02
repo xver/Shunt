a402b4b09c548de3f0336d42e055e4ccbeb1ec46
xver <victor.besyakov@gmail.com>
2021-01-02 08:42:43 -0600

    add make USER_OPT (example: USER_OPT='-g3')and typo+warning fixes

--- examples/sc/LT_simple_sv/initiator/makedir/subdir.mk
+++ examples/sc/LT_simple_sv/initiator/makedir/subdir.mk
@@ -20,7 +20,7 @@ CPP_DEPS += \
 %.o: ../src/%.cpp
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C++ Compiler'
-	g++ -I$(SYSTEMC_HOME) -I$(SYSTEMC_HOME)/include -I$(SHUNT_HOME)/utils/sc/src -I$(SHUNT_HOME)/utils/c/include -I$(SHUNT_HOME)/utils/c/src -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+	g++ $(USER_OPT)  -I$(SYSTEMC_HOME) -I$(SYSTEMC_HOME)/include -I$(SHUNT_HOME)/utils/sc/src -I$(SHUNT_HOME)/utils/c/include -I$(SHUNT_HOME)/utils/c/src -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
