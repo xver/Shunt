6c5b82ea8bb50eb325fd80dc09eff33cf25c3f1f
v <victor.besyakov@gmail.com>
2021-04-03 09:28:34 -0400

    update: cpp (interim 2) & doc & vcs not supported DPI exclude header & cleanup 	modified:   Working Data/CodeDB.nd 	modified:   Working Data/Files.nd 	modified:   Working Data/Output/BuildState.nd 	modified:   Working Data/Output/SearchIndex.nd 	modified:   files/shunt_client_server-h-Summary.js 	modified:   files/shunt_client_server-h-SummaryToolTips.js 	modified:   files/shunt_client_server-h-ToolTips.js 	modified:   files/shunt_client_server-h.html 	modified:   files/shunt_primitives-h-Summary.js 	modified:   files/shunt_primitives-h-SummaryToolTips.js 	modified:   files/shunt_primitives-h-ToolTips.js 	modified:   files/shunt_primitives-h.html 	modified:   files/shunt_user_api-h-Summary.js 	modified:   files/shunt_user_api-h-SummaryToolTips.js 	modified:   files/shunt_user_api-h.html 	modified:   files2/shunt_dpi-h-ToolTips.js 	modified:   files2/shunt_dpi-h.html 	modified:   files3/shunt_dpi_pkg-sv-ToolTips.js 	modified:   files3/shunt_dpi_pkg-sv.html 	modified:   files4/shunt_tlm-h-ToolTips.js 	modified:   files4/shunt_tlm-h.html 	modified:   menu/files.js 	modified:   search/index.js 	modified:   search/keywords/0030.js 	modified:   search/keywords/0063006f006d.js 	modified:   search/keywords/00630073005f.js 	modified:   search/keywords/006400610074.js 	modified:   search/keywords/006500780063.js 	modified:   search/keywords/007300680075.js 	modified:   search/keywords/0074006c006d.js 	modified:   ../utils/c/include/shunt_client_server.h 	modified:   ../utils/c/include/shunt_primitives.h 	modified:   ../utils/sc/src/shunt_tlm.h 	files/shunt_typedef-h-Summary.js 	files/shunt_typedef-h-SummaryToolTips.js 	files/shunt_typedef-h-ToolTips.js 	files/shunt_typedef-h.html 	search/keywords/006400650066.js 	../utils/c/include/shunt_typedef.h 	../utils/dpi/include/shunt_vcs_dpi.svh

--- doc/search/keywords/0030.js
+++ doc/search/keywords/0030.js
@@ -1,1 +1,1 @@
-﻿NDSearch.OnPrefixDataLoaded("0",["Section"],[["0",,[[,"Data TLM2.0 utils",,,0,"File3:shunt_dpi_pkg.sv:Data_TLM2.0_utils"],[,"TLM2.0 utils",,,0,"File:shunt_client_server.h:TLM2.0_utils"]]]]);
+﻿NDSearch.OnPrefixDataLoaded("0",["Section"],[["0",,[[,"Data TLM2.0 utils",,,0,"File3:shunt_dpi_pkg.sv:Data_TLM2.0_utils"],[,"TLM2.0 defines",,,0,"File:shunt_typedef.h:TLM2.0_defines"],[,"TLM2.0 utils",,,0,"File:shunt_client_server.h:TLM2.0_utils"]]]]);
